e514f598cd7fcd1c70c408373f544654ce44be6f
Azam <146977640+aliaxam153@users.noreply.github.com>
2024-06-07 12:02:59 -0700

    Add files via upload

---
+++ ORB_SLAM3/build/CMakeFiles/mono_inertial_tum_vi.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/aazam/dev/ORB_SLAM3/Examples/Monocular-Inertial/mono_inertial_tum_vi.cc" "/home/aazam/dev/ORB_SLAM3/build/CMakeFiles/mono_inertial_tum_vi.dir/Examples/Monocular-Inertial/mono_inertial_tum_vi.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "COMPILEDWITHC11"
+  "HAVE_EIGEN"
+  "HAVE_GLEW"
+  "PANGO_DEFAULT_WIN_URI=\"wayland\""
+  "ROSCONSOLE_BACKEND_LOG4CXX"
+  "ROS_BUILD_SHARED_LIBS=1"
+  "ROS_PACKAGE_NAME=\"ORB_SLAM3\""
+  "_LINUX_"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/ros/noetic/include"
+  "/opt/ros/noetic/share/xmlrpcpp/cmake/../../../include/xmlrpcpp"
+  "/usr/include/opencv4"
+  "../"
+  "../include"
+  "../include/CameraModels"
+  "../Thirdparty/Sophus"
+  "/usr/include/eigen3"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/aazam/dev/ORB_SLAM3/build/CMakeFiles/ORB_SLAM3.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
